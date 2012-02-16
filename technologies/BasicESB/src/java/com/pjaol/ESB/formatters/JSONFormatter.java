04f705a592d23785b3f08c4429ab4f706ab0b20a
pjaol <pjaol@pjaol.com>
2012-02-16 08:26:20 -0500

    modifying JSON Formatter to return a JSON Object, rather than a JSON Array

--- technologies/BasicESB/src/java/com/pjaol/ESB/formatters/JSONFormatter.java
+++ technologies/BasicESB/src/java/com/pjaol/ESB/formatters/JSONFormatter.java
@@ -1,6 +1,7 @@
 package com.pjaol.ESB.formatters;
 
 import org.apache.solr.common.util.NamedList;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
@@ -9,8 +10,52 @@ public class JSONFormatter extends Formatter {
 	@Override
 	public String toOutput(NamedList output) {
 
-		String jsonText = JSONValue.toJSONString(output);
-		return jsonText;
+		return iterateNamedList(output);
+	}
+	
+	
+	private String iterateNamedList(NamedList output){
+		JSONObject jo = new JSONObject();
+		int sz = output.size();
+		
+		for(int i =0; i< sz; i++){
+			String k = output.getName(i);
+			
+			Object v = output.getVal(i);
+			if (v instanceof NamedList){
+				
+				jo.put(k, recurseNamedList((NamedList)v));
+			} else {
+				jo.put(k, v);
+			}
+		}
+		
+		return jo.toJSONString();
+	}
+	
+	private JSONArray recurseNamedList(NamedList output){
+		
+		
+		int sz = output.size();
+		JSONArray jarr = new JSONArray();
+		
+		for(int i =0; i< sz; i++){
+			JSONObject jo = new JSONObject();
+			
+			String k = output.getName(i);
+				
+			Object v = output.getVal(i);
+			if (v instanceof NamedList){
+				
+				jo.put(k, recurseNamedList((NamedList)v));
+			} else {
+				jo.put(k, v);
+			}
+			jarr.add(jo);
+			
+		}
+		
+		return jarr;
 	}
 
 }
